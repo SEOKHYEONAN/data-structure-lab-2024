a35b1408488beddc687278cec05f6385765836ae
SEOKHYEONAN <ash2002@hanamail.net>
2024-04-03 11:54:48 +0900

    23

---
+++ chap02_Array_02/Chap02_class_example/Complex.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include<cstdio>
+
+class Complex
+{
+	double real;
+	double imag;
+public:
+	void set(double r, double i) {
+		real = r;
+		imag = i;
+
+	}
+	void read(const char* msg = "복소수 =") {
+		printf(" %s ", msg);
+		scanf_s("%1f%1f", &real, &imag);
+	}
+	void print(const char* msg "복소수 =") {
+		printf(" %s $4.2f + %4.2fi")
+	}
+};
---
+++ chap02_Array_02/chap02_Array/array_param.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#define
+
+void sub(int x, int arr[]) {
+	x = 10;
+	arr[0] = 10;
+}
+void main()
+{
+	int var:
+	int list[MAX_SIZE];
+
+	var = 0:
+	list[0] = 0;
+	sub(var, list);
+	printf("var=%d, list[0]=%d\n", var, list[0])
+}
