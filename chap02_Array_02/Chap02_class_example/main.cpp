f6365103ffcd16d6342078d05fc22e726816b0a3
SEOKHYEONAN <ash2002@hanamail.net>
2024-04-15 12:01:08 +0900

    array02

--- chap02_Array_02/Chap02_class_example/main.cpp
+++ chap02_Array_02/Chap02_class_example/main.cpp
@@ -16,6 +16,45 @@ int main() {
 	std::cout << "Area:" << r. getarea() << std::endl;
 	std::cout << "Perimeter"<< perimeter << std::endl;
 	std::cout << "is square:" << std::endl;
+
+	// Smarthome class
+	SmartHome myhome("¾È¼®Çö", 23, 40, true);
+	myhome.printStatus();
+
+
+	//Music Streaming Service 
+	MusicStreamingService myService("Spotify");
+	myService.addMusic("VIBE", "Taeyang", "Album", 2023);
+	myService.addMusic("Ditto", "NewJeans", "Album", 2023);
+	myService.addMusic("Attention", "NewJeans", "Album", 2023);
+
+	string music_title;
+	cout << "Enter the Music Title : ";
+	cin >> music_title;
+
+	Music* result = myService.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "found: " << result->getTitle() << " by " << result->getArtist() << endl;
+
+	}
+	else {
+		cout << "Not found" << endl;
+	}
+
+	string artist_name;
+	cout << "Enter the Artist Name : ";
+	cin >> artist_name;
+
+	vector<Music>* > artistResult = myService.searchByArtist(artist_name);
+	if (artistResult.size() > 0) {
+		cout << "Found " << artistResult.size() << " songs by " << artist_name << " : " << endl;
+		for (int i = 0; i < artistResult.size(); i++) {
+			cout << artistResult[i]->getTitle() << endl;
+		}
+	}
+	else {
+		cout << "not found" << endl;
+	}
 }
 
 
